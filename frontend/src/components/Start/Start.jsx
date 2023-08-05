39a9dabb6f46b1ecc1b1497e915cf324601b5477
FloyzenCode <vasyplay323@gmail.com>
2023-08-05 18:29:51 +0400

    Update Start component

--- frontend/src/components/Start/Start.jsx
+++ frontend/src/components/Start/Start.jsx
@@ -27,11 +27,16 @@ function Start() {
 
       {/* overview */}
       <div className="start-overview">
-        <div className="image-abstract"><br /><br /><br /><br />
+        <div className="image-abstract">
           <img src="./home_abstract.webp" alt="" />
         </div>
         <div className="text-about-course">
           <section>
+            <div className="card">
+              <div className="card-icon"><img src="./icons/frontend.svg" alt="" /></div>
+              <h2 className="card-title">Introudction</h2>
+              <p className="card-description">Ознакомьтесь с основными терминами <br />и другими вещами </p>
+            </div>
             <div className="card">
               <div className="card-icon"><img src="./icons/frontend.svg" alt="" /></div>
               <h2 className="card-title">Frontend</h2>
@@ -49,22 +54,42 @@ function Start() {
             </div>
             <div className="card">
               <div className="card-icon"><img src="./icons/frontend.svg" alt="" /></div>
-              <h2 className="card-title">Заголовок карточки 4</h2>
-              <p className="card-description">Описание карточки 4</p>
+              <h2 className="card-title">Languages</h2>
+              <p className="card-description">Выберите свой рабочий язык под <br />свои цели и задачи</p>
             </div>
             <div className="card">
               <div className="card-icon"><img src="./icons/frontend.svg" alt="" /></div>
-              <h2 className="card-title">Заголовок карточки 4</h2>
-              <p className="card-description">Описание карточки 4</p>
-            </div>
-            <div className="card">
-              <div className="card-icon"><img src="./icons/frontend.svg" alt="" /></div>
-              <h2 className="card-title">Заголовок карточки 4</h2>
-              <p className="card-description">Описание карточки 4</p>
+              <h2 className="card-title">More</h2>
+              <p className="card-description">Дальше - больше! Изучите дорожные <br />карты по изучению</p>
             </div>
           </section>
         </div>
       </div>
+
+      {/* footer */}
+      <footer className="start-footer">
+        <div className="start-container-footer">
+          <div className="footer-left">
+            <h1>Russian Tech - new features!</h1>
+            <h4>Платформа для обучения программирования</h4>
+          </div>
+          <div className="footer-right">
+            <p align="center">Social media</p>
+            <div className="social-media-icons">
+              <a href="/"><img src="./icons/github.svg" alt="" /></a>
+              <a href="/"><img src="./icons/vk.svg" alt="" /></a>
+              <a href="/"><img src="./icons/telegram.svg" alt="" /></a>
+            </div>
+            <br /><hr /><br />
+            <div className="footer-email">
+              <p align="center">email: </p>
+              <a href="mailto:russiantechorg@gmail.com?subject=Письмо Russian Tech">russiantechorg@gmail.com</a>
+            </div>
+            <br /><hr /><br />
+            <h3 align="center">Happy coding!</h3> <br />
+          </div>
+        </div>
+      </footer>
     </div>
   )
 }
