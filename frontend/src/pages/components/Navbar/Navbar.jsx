5dc7fc38af1d69fc4dbdea078915b5d9e7604faa
FloyzenCode <vasyplay323@gmail.com>
2023-08-10 00:09:07 +0400

    Update Navbar style

--- frontend/src/pages/components/Navbar/Navbar.jsx
+++ frontend/src/pages/components/Navbar/Navbar.jsx
@@ -2,18 +2,23 @@ import React from 'react'
 import './Navbar.css'
 
 function Navbar() {
-  return (
-    <header className='header'>
-        <h1 className="logo">Russian Tech</h1>
-
-        <nav className="nav-items">
-            <li><a href="/">Главная</a></li>
-            <li><a href="/">Обучение</a></li>
-            <li><a href="/">Блог</a></li>
-            <li><a href="/form">Связь</a></li>
-        </nav>
-    </header>
-  )
-}
+	return (
+		<header class="header">
+			<nav>
+				<div class="logo">
+					<a href="/">Russian<span>Tech</span></a>
+				</div>
+				<input type="checkbox" id="menu-toggle" />
+				<label for="menu-toggle" class="menu-icon">&#9776;</label>
+				<ul class="menu">
+					<li><a href="/">Главная</a></li>
+					<li><a href="/">О нас</a></li>
+					<li><a href="/">Блог</a></li>
+					<li><a href="/">Связь</a></li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
 
 export default Navbar
