7188f8f22f5395e157f8b04f3f0489e6bbd1e0f7
FloyzenCode <vasyplay323@gmail.com>
2023-08-10 18:57:10 +0400

    Add Loader component

---
+++ frontend/src/pages/components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Loader.css'
+
+function Loader() {
+    return (
+        <div className="loader-page">
+            <div class="loader">
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
