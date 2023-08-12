2eac08d3d026361c0c6a99162be8f8a5dade2ab4
FloyzenCode <vasyplay323@gmail.com>
2023-08-12 12:35:45 +0400

    Update code

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -6,6 +6,8 @@ import Loader from '../../src/components/Loader/Loader'
 import Home from '../pages/Home/Home'
 import About from '../pages/About/About'
 import Blog from '../pages/Blog/Blog';
+import AdminDashboard from '../pages/Admin/AdminDashboard';
+import Feedback from '../pages/Feedback/Feedback';
 
 function Router() {
   return (
@@ -15,10 +17,11 @@ function Router() {
         <Route path='/home' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/blog' element={<Blog />} />
+        <Route path='admin' element={<AdminDashboard />} />
+        <Route path='/feedback' element={<Feedback />} />
         <Route path='*' element={<div>404</div>} />
       </Routes>
     </Suspense>
-
   )
 }
 
