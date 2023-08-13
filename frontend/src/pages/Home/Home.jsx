0758a178c769f6395faaa5305eb92a0eb3371998
FloyzenCode <vasyplay323@gmail.com>
2023-08-14 01:06:28 +0400

    Add routes for attributes

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -17,77 +17,77 @@ function Home() {
       title: "Введение",
       description: "Введение в основные понятия и терминологии",
       hoverText: "Познакомьтесь с миром IT",
-      to: '/home'
-      },
-      {
+      to: '/language/introduction'
+    },
+    {
       imageSrc: "./icons/languages/python.svg",
       title: "Python",
       description: "Введение в язык и принципы работы с ним",
       hoverText: "Обучение Python с нуля",
-      to: '/home',
-      },
-      {
+      to: '/language/python',
+    },
+    {
       imageSrc: "./icons/languages/cplusplus.svg",
       title: "C++",
       description: "Основы языка C++ и его применение",
       hoverText: "Изучите C++ с нуля",
-      to: '/home',
-      },
-      {
+      to: '/language/cpp',
+    },
+    {
       imageSrc: "./icons/languages/javascript.svg",
       title: "JS",
       description: "Основы языка JavaScript и его применение",
       hoverText: "Изучите JavaScript с нуля",
-      to: '/home',
-      },
-      {
+      to: '/language/javascript',
+    },
+    {
       imageSrc: "./icons/languages/swift.svg",
       title: "Swift",
       description: "Введение в язык Swift и его возможности",
       hoverText: "Изучите Swift с нуля",
-      to: '/home',
-      },
-      {
+      to: '/language/swift',
+    },
+    {
       imageSrc: "./icons/languages/csharp.svg",
       title: "C#",
       description: "Основы языка C# и его применение",
       hoverText: "Изучите C# с нуля",
-      to: '/home',
-      },
-      {
+      to: '/language/csharp',
+    },
+    {
       imageSrc: "./icons/languages/html_css.svg",
       title: "HTML & CSS",
       description: "Основы HTML и CSS для создания веб-страниц",
       hoverText: "Изучите HTML & CSS с нуля",
-      to: '/home',
-      },
-      {
+      to: '/language/htmlcss',
+    },
+    {
       imageSrc: "./icons/languages/php.svg",
       title: "PHP",
       description: "Введение в язык PHP и создание динамических веб-сайтов",
       hoverText: "Изучите PHP с нуля",
-      to: '/home',
-      },
-      {
+      to: '/language/php',
+    },
+    {
       imageSrc: "./icons/languages/java.svg",
       title: "Java",
       description: "Основы языка Java и его применение",
       hoverText: "Изучите Java с нуля",
-      to: '/home',
-      },
-      {
+      to: '/language/java',
+    },
+    {
       imageSrc: "./icons/languages/kotlin.svg",
       title: "Kotlin",
       description: "Введение в язык Kotlin и его особенности",
       hoverText: "Изучите Kotlin с нуля",
-      to: '/home',
-      },
-      {
+      to: '/language/kotlin',
+    },
+    {
       imageSrc: "./icons/languages/rust.svg",
       title: "Rust",
       description: "Основы языка Rust и его применение",
       hoverText: "Изучите Rust с нуля",
-      to: '/home',
+      to: '/language/rust',
     }
   ];
 
