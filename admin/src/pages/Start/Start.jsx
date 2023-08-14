538c72a187c3a0164dc1dd7612737264a37cc0c0
FloyzenCode <vasyplay323@gmail.com>
2023-08-15 02:25:45 +0400

    Failed test try x2

--- admin/src/pages/Start/Start.jsx
+++ admin/src/pages/Start/Start.jsx
@@ -1,23 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Start.css'
 
 function Start() {
-  const apiUrl = "http://api.localhost/file?path=db.json"
+  // const apiUrl = "http://api.localhost/file?path=db.json"
+  const apiUrl = "http://localhost/get?source=external&label=test"
+  const [backendData, setBackend] = useState([{}])
   // const apiUrl = "https://jsonplaceholder.typicode.com/todos/"
-  function pullJson() {
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(responseData => {
-        console.log(responseData)
-      })
-    // return
-  }
+
   useEffect(() => {
-    pullJson()
-  })
+    fetch(apiUrl).then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackend(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
   return (
     <div>
       <h1>Start</h1>
+      {(typeof backendData.external === 'undefined') ? (<h2>Loading...</h2>) : (
+        backendData.users.map((data, i) => (
+          <p className='listUsers' key={i}>{i + 1}) {data}</p>
+        ))
+      )}
+
     </div>
   )
 }
