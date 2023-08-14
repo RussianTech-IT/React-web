9199ec4771581772b43d5a18736cd8b3d9faaff0
FloyzenCode <vasyplay323@gmail.com>
2023-08-15 01:48:10 +0400

    test failed try

--- admin/src/pages/Start/Start.jsx
+++ admin/src/pages/Start/Start.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Start.css'
 
 function Start() {
-  
+  const apiUrl = "http://api.localhost/file?path=db.json"
+  // const apiUrl = "https://jsonplaceholder.typicode.com/todos/"
+  function pullJson() {
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(responseData => {
+        console.log(responseData)
+      })
+    // return
+  }
+  useEffect(() => {
+    pullJson()
+  })
   return (
     <div>
       <h1>Start</h1>
-
     </div>
   )
 }
