39f9d3e1e51756a55127554967442a94de046572
JustNyan <justnyan@devuan.beeline>
2023-08-15 02:04:05 +0400

    fix back CORS enable

--- backend/volumes/api/flask/main.py
+++ backend/volumes/api/flask/main.py
@@ -1,22 +1,29 @@
 #!flask/bin/python
 import api
 from flask import Flask, jsonify, send_file
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route("/")
+@cross_origin()
 def main(): return api.main()
 
 
-@app.route("/file",methods=["GET"])
+@app.route("/file")
+@cross_origin()
 def file(): return api.file()
 
 @app.route("/get")
+@cross_origin()
 def get(): return api.get()
 
 @app.route("/auth")
+@cross_origin()
 def auth(): return api.auth()
 
 
