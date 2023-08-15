fff69ce44e5d0209d82187fb67ac0c60f82c8f52
FloyzenCode <vasyplay323@gmail.com>
2023-08-15 17:50:21 +0400

    Test requests succes & admin panel route

--- backend/server/server.js
+++ backend/server/server.js
@@ -3,11 +3,14 @@ const app = express()
 const PORT = 5000
 const jsonFile = require("../database/data.json")
 
+// security send requiests header CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 })
+
+// admin panel
 
 app.get('/api', (req, res) => {
     res.json("You inside API")
--- frontend/src/pages/Blog/Blog.jsx
+++ frontend/src/pages/Blog/Blog.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Blog.css'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
+// import Loader from '../../components/Loader/Loader'
 
 function Blog() {
+  const [backendData, setBackend] = useState([{}])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/users").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackend(data)
+      }
+    )
+  }, [])
+
   return (
     <div className="blog-page">
       <Navbar />
       <div>Blog</div>
+      {(typeof backendData.users === 'undefined') ? (
+        <h1>Loading...</h1>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p className='listUsers' key={i}>{i + 1}) {user}</p>
+        ))
+      )}
+
       <Footer />
     </div>
   )
